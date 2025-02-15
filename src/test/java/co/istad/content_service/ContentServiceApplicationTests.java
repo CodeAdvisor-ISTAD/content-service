805fa5dd309efc169ac9d855dddde8cf43acfa37
MengseuThoeng <mengseu2004@gmail.com>
2025-02-15 15:22:44 +0700

    Test Git

--- src/test/java/co/istad/content_service/ContentServiceApplicationTests.java
+++ src/test/java/co/istad/content_service/ContentServiceApplicationTests.java
@@ -1,13 +1,13 @@
-package co.istad.content_service;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-//@SpringBootTest
-//class ContentServiceApplicationTests {
+//package co.istad.content_service;
 //
-////	@Test
-////	void contextLoads() {
-////	}
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
 //
-//}
+////@SpringBootTest
+////class ContentServiceApplicationTests {
+////
+//////	@Test
+//////	void contextLoads() {
+//////	}
+////
+////}
